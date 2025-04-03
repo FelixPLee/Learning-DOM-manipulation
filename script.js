d7da4d7cebeeca99c33ebaa2caffde0720e90876
FelixPLee <felixpinheirolee@gmail.com>
2025-04-03 13:20:29 -0300

    Learning, cookie, selecting

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,32 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////////////
+/////////////////Aprendendo/////////////////
+////////////////////////////////////////////
+
+console.log(document.documentElement)
+'console.log(document)'
+
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('.selection')
+console.log(allSections)
+
+document.getElementById('section--1')
+const allButtons = document.getElementsByTagName('button')
+console.log(allButtons)
+
+console.log(document.getElementsByClassName('btn'))
+
+//Creating and inserting elements
+// . insertAdjacentHTML
+
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+//message.textContent = 'We use cookies for improved functionality and analytics.'
+message.innerHTML = 'We use cookies for improved functionality and analytics.<button class="btn btn--close-cookie">Got it</button>'
+
+//header.prepend(message)
+header.append(message)
