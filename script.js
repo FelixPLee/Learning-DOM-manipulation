836da774c0b04bef83a2b2cb7171752bc92a8e46
Felix Pinheiro Lee <felixpinheirolee@gmail.com>
2025-04-12 19:48:36 -0300

    Rustic way to make nav sticky

--- script.js
+++ script.js
@@ -115,6 +115,16 @@ const handleHover = function (e) {
 // passing 'argument' into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5))
 nav.addEventListener('mouseout', handleHover.bind(1))
+
+//Stity nav
+const initialCoords = section1.getBoundingClientRect()
+
+window.addEventListener('scroll', function () {
+  if(this.window.scrollY > initialCoords.top) nav.
+  classList.add('sticky')
+  else nav.classList.remove('sticky')
+})
+
 ////////////////////////////////////////////
 /////////////////Aprendendo/////////////////
 ////////////////////////////////////////////
