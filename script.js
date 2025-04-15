7a0e5b6ebe891046b0f4eb9e1e43159a87aaa267
FelixPLee <felixpinheirolee@gmail.com>
2025-04-15 07:53:04 -0300

    Slider functional

--- script.js
+++ script.js
@@ -46,66 +46,66 @@ document.addEventListener('keydown', function (e) {
 btnScrollTo.addEventListener('click', function(e) {
   const s1coords = section1.getBoundingClientRect()
   console.log(e.target.getBoundingClientRect())
-
-//valores de scroll do topo e da lateral 
+  
+  //valores de scroll do topo e da lateral 
   // console.log(window. scrollX, scrollY)
   // console.log(document.documentElement.clientHeight)
   // console.log(document.documentElement.clientWidth)
   // console.log(s1coords.left)
   // console.log(s1coords.top)
   // window.scrollTo({
-  //   left: s1coords.left + window.scrollX,
-  //   top: s1coords.top + scrollY,
-  //   behavior: 'smooth'
-  // })
-  section1.scrollIntoView({behavior: 'smooth'})
-})
-
-//document.querySelectorAll('.nav__link').forEach(function(el) {
-//  el.addEventListener('click', function(e) {
-//    e.preventDefault()
-//    const id = this.getAttribute('href')
-//    console.log(id)
-//    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
-//  })
-//})
-
-// 1. add event listener to comon parent element
-// 2. Dertemine what element originated the event
-
-document.querySelector('.nav__links').addEventListener('click', function(e) {
-  e.preventDefault()
-  if(e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href')
-    console.log(id)
-    document.querySelector(id).scrollIntoView({behavior: 'smooth'})   
-  }
-})
-
-
-tabsContainer.addEventListener('click', function(e) {
-  const clicked = e.target.closest('.operations__tab')
-  if(!clicked) return;
-
-  tabs.forEach(t => t.classList.remove('operations__tab--active'))
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
-
-  //Activate tab
-  clicked.classList.add('operations__tab--active')
-
-  //Activate content area
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
-  .classList.add('operations__content--active')
-})
-
-// menu fade
-const handleHover = function (e) {
-  if(e.target.classList.contains('nav__link')) {
-    const link = e.target
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link')
-    const logo = link.closest('.nav').querySelector('img')
-    
-    siblings.forEach(el => {
+    //   left: s1coords.left + window.scrollX,
+    //   top: s1coords.top + scrollY,
+    //   behavior: 'smooth'
+    // })
+    section1.scrollIntoView({behavior: 'smooth'})
+  })
+  
+  //document.querySelectorAll('.nav__link').forEach(function(el) {
+    //  el.addEventListener('click', function(e) {
+      //    e.preventDefault()
+      //    const id = this.getAttribute('href')
+      //    console.log(id)
+      //    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+      //  })
+      //})
+      
+      // 1. add event listener to comon parent element
+      // 2. Dertemine what element originated the event
+      
+      document.querySelector('.nav__links').addEventListener('click', function(e) {
+        e.preventDefault()
+        if(e.target.classList.contains('nav__link')) {
+          const id = e.target.getAttribute('href')
+          console.log(id)
+          document.querySelector(id).scrollIntoView({behavior: 'smooth'})   
+        }
+      })
+      
+      
+      tabsContainer.addEventListener('click', function(e) {
+        const clicked = e.target.closest('.operations__tab')
+        if(!clicked) return;
+        
+        tabs.forEach(t => t.classList.remove('operations__tab--active'))
+        tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+        
+        //Activate tab
+        clicked.classList.add('operations__tab--active')
+        
+        //Activate content area
+        document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add('operations__content--active')
+      })
+      
+      // menu fade
+      const handleHover = function (e) {
+        if(e.target.classList.contains('nav__link')) {
+          const link = e.target
+          const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+          const logo = link.closest('.nav').querySelector('img')
+          
+          siblings.forEach(el => {
       if(el !== link) el.style.opacity = this
     })
     logo.style.opacity = this
@@ -122,7 +122,7 @@ const navHeight =nav.getBoundingClientRect().height
 const stickyNav = function (entries) {
   const [entry] = entries
   if (!entry.isIntersecting) nav.classList.add('sticky')
-  else nav.classList.remove('sticky')
+    else nav.classList.remove('sticky')
 }
 
 const headerObserver = new IntersectionObserver
@@ -162,7 +162,7 @@ const loadImg = function(entries, observer) {
   const [entry] = entries
   console.log(entry)
   if(!entry.isIntersecting) return
-
+  
   //replace src with data-src
   entry.target.src = entry.target.dataset.src
   entry.target.addEventListener('load', function() {})
@@ -179,6 +179,34 @@ const imgObserver = new IntersectionObserver
 
 imgTargets.forEach(img => imgObserver.observe(img))
 
+//Slider
+const slides = document.querySelectorAll('.slide')
+const slider = document.querySelector('.slider')
+const btnRight = document.querySelector('.slider__btn--right')
+const btnLeft = document.querySelector('.slider__btn--left')
+
+slider.style.overflow = 'visible(0.5)'
+
+let curSlide = 0
+const maxSlide = slides.length
+
+const goToSlide = function(slide) {
+  slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
+}
+goToSlide(0)
+
+const nextSlide = function() {
+  if (curSlide === maxSlide -1) curSlide = 0
+  else curSlide++ 
+  goToSlide(curSlide)
+}
+const prevSlide = function(){ 
+  if (curSlide === 0) curSlide = maxSlide -1
+  else curSlide--
+  goToSlide(curSlide)
+}
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
 // const initialCoords = section1.getBoundingClientRect()
 
 // window.addEventListener('scroll', function () {
