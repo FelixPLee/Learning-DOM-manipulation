fd69a73916cb028f2f8f1b4014bde61feb97b2c5
FelixPLee <felixpinheirolee@gmail.com>
2025-04-15 08:57:12 -0300

    Slider complete

--- script.js
+++ script.js
@@ -179,42 +179,85 @@ const imgObserver = new IntersectionObserver
 
 imgTargets.forEach(img => imgObserver.observe(img))
 
-//Slider
+//  Slider
+const slider = function() {
 const slides = document.querySelectorAll('.slide')
 const slider = document.querySelector('.slider')
 const btnRight = document.querySelector('.slider__btn--right')
 const btnLeft = document.querySelector('.slider__btn--left')
+const dotContainer = document.querySelector('.dots')
 
 slider.style.overflow = 'visible(0.5)'
 
 let curSlide = 0
 const maxSlide = slides.length
 
+
+// Slider Functions
+
+const createDots = function () {
+  slides.forEach(function(_, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    )
+  })
+}
+
+const activateDot = function(slide) {
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'))
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active')
+}
+
 const goToSlide = function(slide) {
   slides.forEach((s, i) => s.style.transform = `translateX(${100 * (i - slide)}%)`)
 }
-goToSlide(0)
+const init = function(){
+  goToSlide(0)
+  createDots()
+  activateDot(0)
+}
+init()
 
 const nextSlide = function() {
   if (curSlide === maxSlide -1) curSlide = 0
   else curSlide++ 
   goToSlide(curSlide)
+  activateDot(curSlide)
 }
 const prevSlide = function(){ 
   if (curSlide === 0) curSlide = maxSlide -1
   else curSlide--
   goToSlide(curSlide)
+  activateDot(curSlide)
 }
+
+////// Slide event handler
 btnRight.addEventListener('click', nextSlide)
 btnLeft.addEventListener('click', prevSlide)
+
+document.addEventListener('keydown', function(e) {
+  if(e.key === 'ArrowLeft') prevSlide()
+    e.key === 'ArrowRight' && nextSlide()
+})
+
+dotContainer.addEventListener('click', function (e) {
+  if(e.target.classList.contains("dots__dot")) {
+    curSlide = Number(e.target.dataset.slide)
+    goToSlide(curSlide)
+    activateDot(curSlide)
+  }
+})
+}
+slider()
 // const initialCoords = section1.getBoundingClientRect()
 
 // window.addEventListener('scroll', function () {
-//   if(this.window.scrollY > initialCoords.top) nav.
-//   classList.add('sticky')
-//   else nav.classList.remove('sticky')
-// })
-
+  //   if(this.window.scrollY > initialCoords.top) nav.
+  //   classList.add('sticky')
+  //   else nav.classList.remove('sticky')
+  // })
+  
 ////////////////////////////////////////////
 /////////////////Aprendendo/////////////////
 ////////////////////////////////////////////
